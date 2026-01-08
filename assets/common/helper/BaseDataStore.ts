672205e6c08db694702ce4e2aaa05a7976535948
Thong Doan <dmthong181096@gmail.com>
2026-01-08 10:30:53 +0700

    fox format data store

--- assets/common/helper/BaseDataStore.ts
+++ assets/common/helper/BaseDataStore.ts
@@ -1,24 +1,37 @@
-import { _decorator, Component , sys} from 'cc';
-const { ccclass, property } = _decorator;
+import * as cc from 'cc';
+const { ccclass } = cc._decorator;
 
 @ccclass('BaseDataStore')
-export class BaseDataStore extends Component {
-
-
-    // protected onLoad(): void {
-    //     this.defineKey();
-    // }
+export class BaseDataStore extends cc.Component {
 
     protected gameID = "";
 
-    setLocalStoreByKey(key, value){
-        sys.localStorage.setItem(this.gameID + key, value);
-    }
-    getLocalStoreByKey(key){
+    setLocalStoreByKey(key: string, value: any): void {
         if(this.gameID == ""){
             this.defineKey();
         }
-        return sys.localStorage.getItem(this.gameID + key) || 0;
+        const existingData = this.getAllData();
+        existingData[key] = value;
+        cc.sys.localStorage.setItem(this.gameID, JSON.stringify(existingData));
+    }
+    
+    getLocalStoreByKey(key: string): any {
+        if(this.gameID == ""){
+            this.defineKey();
+        }
+        
+        const data = this.getAllData();
+        return data[key] || 0;
+    }
+    
+    private getAllData(): any {
+        const dataString = cc.sys.localStorage.getItem(this.gameID) || '{}';
+        try {
+            return JSON.parse(dataString);
+        } catch (e) {
+            cc.warn(`Failed to parse localStorage data for ${this.gameID}:`, e);
+            return {};
+        }
     }
 
     defineKey(){
